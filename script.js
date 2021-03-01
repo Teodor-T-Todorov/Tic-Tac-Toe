da9a3e190e2019677c6d63471cb8bacd4da14b66
TheodorTodorov <teodor.t.todorov@outlook.com>
2021-03-01 21:13:55 +0200

    Added 2 modes, AI still not ready.

--- script.js
+++ script.js
@@ -1,27 +1,60 @@
+//Query selectors
 const board = document.querySelector('#board');
 const cells = document.querySelectorAll('.cell');
 const xTurn = document.querySelector('#xTurn');
 const oTurn = document.querySelector('#oTurn');
 const announcer = document.querySelector('#announcer');
-const overlay = document.querySelector('#overlay');
 const content = document.querySelector('#content');
+const btnReplay = document.querySelector('#btn-replay');
+const btnPlayer = document.querySelector('#btn-player');
+const btnAi = document.querySelector('#btn-ai');
+
+const Player = (sign) =>
+{
+    return {sign};
+};
 
 const gameBoard = ( () =>
 {
     let board = ['', '', '', '', '', '', '', '', ''];
-
+    let turn = 1;
+    let activePlayer = Player('X');
+    
     const makeMove = (position, sign) =>
     {
         board[position] = sign;
     }
 
-    const clearBoard = () =>
+    const switchPlayer = (switchPlayer) =>
     {
+        activePlayer = switchPlayer;
+    }
+    
+    const getPlayer = () =>
+    {
+        return activePlayer;
+    }
+
+    const incrementTurn  = () =>
+    {
+        turn++;
+    }
+
+    const getTurn = () => 
+    {
+        return turn;
+    }
+
+    const restartGame = () =>
+    {
+        activePlayer = Player('X');
+        turn = 1;
         board = ['', '', '', '', '', '', '', '', '']
         cells.forEach((cell) =>
         {
             cell.textContent = '';
         })
+
     }
 
     const winCondition = () => 
@@ -53,95 +86,93 @@ const gameBoard = ( () =>
     }
 
     return{
+        getPlayer,
+        restartGame,
+        switchPlayer,
+        incrementTurn,
+        getTurn,
         makeMove,
-        clearBoard,
         winCondition
     }
 })();
 
-const Player = (name, sign) =>
+const Game = (gameMode) =>
 {
-    return {name, sign};
-};
+    if(gameMode == 'Player')
+    {
+        const player1 = Player('X');
+        const player2 = Player('O');
 
-const Game = (() =>
-{
-    const player1 = Player('Player 1','x');
-    const player2 = Player('Player 2','o');
-    let turn = 1;
-
-    let activePlayer = player1;
-
-    //Add event listener to the squares
-    board.addEventListener('click', function(e){
-        if(e.target.textContent.length == 0) //Check if the current square is empty
-        {
-            turn++;
-            e.target.innerHTML = activePlayer.sign; // Place player's sign on the board
-            gameBoard.makeMove(e.target.id, activePlayer.sign) // Place the sign in the array
-
-            //Switch players
-            if(activePlayer == player1 && gameBoard.winCondition() == false)
+        //Add event listener to the squares
+        board.addEventListener('click', function(e){
+            if(e.target.textContent.length == 0) //Check if the current square is empty
             {
-                activePlayer = player2;
-
-                oTurn.style.color = 'white';
-                oTurn.style.backgroundColor = '#4e4d4d';
-
-                xTurn.style.color = '#4e4d4d';
-                xTurn.style.backgroundColor = 'white';
-            }
-
-            else if(activePlayer == player2 && gameBoard.winCondition() == false)
-            {
-                activePlayer = player1;
-
-                xTurn.style.color = 'white';
-                xTurn.style.backgroundColor = '#4e4d4d';
-
-                oTurn.style.color = '#4e4d4d';
-                oTurn.style.backgroundColor = 'white';
-            }
-        }
-
-        if(turn >= 5 && gameBoard.winCondition()) //First potential win condition comes at 5th turn
-        {
-
-            setTimeout(() => 
-            {
-                announcer.style.display = 'block';
-                content.getElementsByTagName('h1')[0].innerHTML = `${activePlayer.name} won!`;
-                gameBoard.clearBoard();
-                turn = 1;
-                activePlayer = player1;
-                xTurn.style = null;
-                oTurn.style = null;
-            }, 500);
-        }
-        else if(turn == 9) //Then we have a draw
-        {
-            setTimeout(() => 
-            {
-                announcer.style.display = 'block';
-                content.getElementsByTagName('h1')[0].innerHTML = 'Draw!';
-                gameBoard.clearBoard();
-                turn = 1;
-                activePlayer = player1;
-                xTurn.style = null;
-                oTurn.style = null;
-            }, 500);
-        } 
-    })
+                gameBoard.incrementTurn();
+                e.target.innerHTML = gameBoard.getPlayer().sign; // Place player's sign on the board
+                gameBoard.makeMove(e.target.id, gameBoard.getPlayer().sign) // Place the sign in the array
     
-    return {};
-})();
+                //Switch players
+                if(gameBoard.getTurn() % 2 == 0 && gameBoard.winCondition() == false)
+                {
+                    gameBoard.switchPlayer(player2);
+    
+                    oTurn.style.color = 'white';
+                    oTurn.style.backgroundColor = '#4e4d4d';
+    
+                    xTurn.style.color = '#4e4d4d';
+                    xTurn.style.backgroundColor = 'white';
+                }
+    
+                else if(gameBoard.getTurn() % 2 == 1 && gameBoard.winCondition() == false)
+                {
+                    gameBoard.switchPlayer(player1)
+    
+                    xTurn.style.color = 'white';
+                    xTurn.style.backgroundColor = '#4e4d4d';
+    
+                    oTurn.style.color = '#4e4d4d';
+                    oTurn.style.backgroundColor = 'white';
+                }
+            }
+    
+            if(gameBoard.getTurn() >= 5 && gameBoard.winCondition()) //First potential win condition comes at 5th turn
+            {
+                setTimeout(() => 
+                {
+                    announcer.style.display = 'block';
+                    content.getElementsByTagName('h1')[0].innerHTML = `${gameBoard.getPlayer().sign} won!`;
+                    gameBoard.restartGame();
+                    xTurn.style = null;
+                    oTurn.style = null;
+                }, 250);
+            }
+            else if(gameBoard.getTurn() == 10) //Then we have a draw
+            {
+                setTimeout(() => 
+                {
+                    announcer.style.display = 'block';
+                    content.getElementsByTagName('h1')[0].innerHTML = 'Draw!';
+                    gameBoard.restartGame();
+                    xTurn.style = null;
+                    oTurn.style = null;
+                }, 250);
+            } 
+        })
+    }
+}
 
+Game('Player') 
 //Event listeners
 
-overlay.addEventListener('click', (e) => 
+btnPlayer.addEventListener('click', () =>
 {
-    if(e.target.id == 'overlay' && e.target.id != 'content')
-    {
-        announcer.style.display = 'none';  
-    }
+    gameBoard.restartGame();
+    xTurn.style = null;
+    oTurn.style = null;
+    Game('Player')
+})
+
+btnReplay.addEventListener('click', () => 
+{
+    announcer.style.display = 'none';  
 })
