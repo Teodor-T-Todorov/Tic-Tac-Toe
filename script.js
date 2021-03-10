41258080293e648bd0a228aa279693280625855f
TheodorTodorov <teodor.t.todorov@outlook.com>
2021-03-10 16:59:00 +0200

    Added AI using the minimax algorithm, fixed some parts of the code

--- script.js
+++ script.js
@@ -7,169 +7,279 @@ const announcer = document.querySelector('#announcer');
 const content = document.querySelector('#content');
 const btnReplay = document.querySelector('#btn-replay');
 const btnPlayer = document.querySelector('#btn-player');
-const btnAi = document.querySelector('#btn-ai');
+const btnAI = document.querySelector('#btn-ai');
 
 const Player = (sign) =>
 {
     return {sign};
 };
 
-const gameBoard = ( () =>
+const playerX = Player('X');
+const playerO = Player('O');
+
+let gameModes = {
+    vsPlayer: 'vsPlayer',
+    vsAI: 'vsAI'
+};
+
+const game = ( () =>   
 {
     let board = ['', '', '', '', '', '', '', '', ''];
     let turn = 1;
-    let activePlayer = Player('X');
+    let activePlayer = playerX; // X starts by default
+    let gameMode = 'vsPlayer'; // vsPlayer mode by default
+
+    const setGameMode = (mode) =>
+    {
+        gameMode = mode;
+        restartGame();
+    }
+
+    const getGameMode = () =>
+    {
+        return gameMode;
+    }
     
     const makeMove = (position, sign) =>
     {
         board[position] = sign;
+
+        //Switch players
+        if(turn % 2 == 1 && winCondition(activePlayer) == false) // Switch the player if there is no winner on the current turn
+        {
+            activePlayer = playerO;
+
+            //Shows visually whose turn is now
+            if(gameMode == 'vsPlayer') // We want to switch colors of current player only when we play vsPlayer
+            {
+                oTurn.style.color = 'white';
+                oTurn.style.backgroundColor = '#4e4d4d';
+                xTurn.style.color = '#4e4d4d';
+                xTurn.style.backgroundColor = 'white';
+            }
+        }
+
+        else if(turn % 2 == 0 && winCondition(activePlayer) == false) // Switch the player if there is no winner on the current turn
+        {
+            activePlayer = playerX;
+
+            //Shows visually whose turn is now
+            xTurn.style.color = 'white';
+            xTurn.style.backgroundColor = '#4e4d4d';
+            oTurn.style.color = '#4e4d4d';
+            oTurn.style.backgroundColor = 'white';
+        }
+
+        turn++;
+
+        if(turn >= 5 && winCondition(activePlayer)) //First potential win condition comes at 5th turn
+        {
+            game.announceWinner()
+        }
+        else if(turn == 10) //Then we have a draw
+        {
+            game.announceDraw();
+        } 
     }
 
-    const switchPlayer = (switchPlayer) =>
+    const aiMakeMove = () =>
     {
-        activePlayer = switchPlayer;
+        if(turn != 1)
+        {
+            let index = bestMove();
+            board[index] = playerO.sign;
+            cells[index].textContent = playerO.sign;
+    
+            turn++;
+    
+            if(turn >= 5 && winCondition(activePlayer)) //First potential win condition comes at 5th turn
+            {
+                game.announceWinner()
+            }
+            else if(turn == 10) //Then we have a draw
+            {
+                game.announceDraw();
+            } 
+        } 
+    }
+
+    const bestMove = () =>
+    {
+        let bestScore = -10000;
+        let bestMove;
+        let score;
+        for(let i = 0; i < board.length; i++)
+        {
+            if(board[i] == '')
+            {
+                board[i] = playerO.sign;
+                turn++;
+                score = minimax(false);
+                board[i] = '';
+                turn--;
+                if(score > bestScore)
+                {
+                    bestScore = score;
+                    bestMove = i;
+                }
+            }
+        }
+        return bestMove;
+    }
+
+    const minimax = (isMax) =>
+    {
+        if(winCondition(playerO))
+        {
+            return 10;
+        }
+        else if(winCondition(playerX))
+        {
+            return -10;
+        }
+        else if(turn == 10)
+        {
+            return 0;
+        }
+
+        if(isMax)
+        {
+            let bestScore = -10000;
+            let score;
+            for(let i = 0; i < board.length; i++)
+            {
+                if(board[i] == '')
+                {
+                    board[i] = playerO.sign;
+                    turn++;
+                    score = minimax(false);
+                    board[i] = '';
+                    turn--
+                    bestScore = Math.max(score,bestScore);
+                }
+            }
+            return bestScore;
+        }
+        else
+        {
+            let bestScore = 10000;
+            let score;
+            for(let i = 0; i < board.length; i++)
+            {
+                if(board[i] == '')
+                {
+                    board[i] = playerX.sign;
+                    turn++;
+                    score = minimax(true);
+                    board[i] = '';
+                    turn--;
+                    bestScore = Math.min(score, bestScore);
+                }
+            }
+            return bestScore;
+        }
     }
     
-    const getPlayer = () =>
+    const getActivePlayer = () =>
     {
         return activePlayer;
     }
 
-    const incrementTurn  = () =>
-    {
-        turn++;
-    }
-
-    const getTurn = () => 
-    {
-        return turn;
-    }
-
     const restartGame = () =>
     {
-        activePlayer = Player('X');
+        activePlayer = playerX;
         turn = 1;
         board = ['', '', '', '', '', '', '', '', '']
         cells.forEach((cell) =>
         {
             cell.textContent = '';
         })
-
+        xTurn.style = null;
+        oTurn.style = null;
     }
 
-    const winCondition = () => 
+    const winCondition = (player) => 
     {
         //Check rows
-        if((board[0] == board[1] && board[1] == board[2] && board[0] != '') ||
-           (board[3] == board[4] && board[4] == board[5] && board[3] != '') ||
-           (board[6] == board[7] && board[7] == board[8] && board[6] != ''))
+        if((board[0] == player.sign && board[1] == player.sign && board[2] == player.sign) || //Check rows
+           (board[3] == player.sign && board[4] == player.sign && board[5] == player.sign) ||
+           (board[6] == player.sign && board[7] == player.sign && board[8] == player.sign) ||
+           (board[0] == player.sign && board[3] == player.sign && board[6] == player.sign) || //Check cols
+           (board[1] == player.sign && board[4] == player.sign && board[7] == player.sign) ||
+           (board[2] == player.sign && board[5] == player.sign && board[8] == player.sign) ||
+           (board[0] == player.sign && board[4] == player.sign && board[8] == player.sign) || //Check diagonals
+           (board[2] == player.sign && board[4] == player.sign && board[6] == player.sign))
         {
             return true;
         }
-
-    //Check cols
-        else if((board[0] == board[3] && board[3] == board[6] && board[0] != '') ||
-                (board[1] == board[4] && board[4] == board[7] && board[1] != '') ||
-                (board[2] == board[5] && board[5] == board[8] && board[2] != ''))
-        {
-            return true;
-        }
-
-    //Check diagonals
-        else if((board[0] == board[4] && board[4] == board[8] && board[0] != '') ||
-                (board[2] == board[4] && board[4] == board[6] && board[2] != ''))
-        {
-            return true;
-        }
+  
 
         return false;
     }
 
+    const announceWinner = () =>
+    {
+        announcer.style.display = 'block';
+        content.getElementsByTagName('h1')[0].innerHTML = `${game.getActivePlayer().sign} won!`;
+        game.restartGame();
+        xTurn.style = null;
+        oTurn.style = null;
+    };
+
+    const announceDraw = () =>
+    {
+        announcer.style.display = 'block';
+        content.getElementsByTagName('h1')[0].innerHTML = 'Draw!';
+        game.restartGame();
+        xTurn.style = null;
+        oTurn.style = null;
+    };
+
     return{
-        getPlayer,
-        restartGame,
-        switchPlayer,
-        incrementTurn,
-        getTurn,
+        setGameMode,
+        getGameMode,
+        getActivePlayer,
         makeMove,
+        minimax,
+        bestMove,
+        aiMakeMove,
+        restartGame,
+        announceWinner,
+        announceDraw,
         winCondition
     }
 })();
-
-const Game = (gameMode) =>
-{
-    if(gameMode == 'Player')
-    {
-        const player1 = Player('X');
-        const player2 = Player('O');
-
-        //Add event listener to the squares
-        board.addEventListener('click', function(e){
-            if(e.target.textContent.length == 0) //Check if the current square is empty
-            {
-                gameBoard.incrementTurn();
-                e.target.innerHTML = gameBoard.getPlayer().sign; // Place player's sign on the board
-                gameBoard.makeMove(e.target.id, gameBoard.getPlayer().sign) // Place the sign in the array
-    
-                //Switch players
-                if(gameBoard.getTurn() % 2 == 0 && gameBoard.winCondition() == false)
-                {
-                    gameBoard.switchPlayer(player2);
-    
-                    oTurn.style.color = 'white';
-                    oTurn.style.backgroundColor = '#4e4d4d';
-    
-                    xTurn.style.color = '#4e4d4d';
-                    xTurn.style.backgroundColor = 'white';
-                }
-    
-                else if(gameBoard.getTurn() % 2 == 1 && gameBoard.winCondition() == false)
-                {
-                    gameBoard.switchPlayer(player1)
-    
-                    xTurn.style.color = 'white';
-                    xTurn.style.backgroundColor = '#4e4d4d';
-    
-                    oTurn.style.color = '#4e4d4d';
-                    oTurn.style.backgroundColor = 'white';
-                }
-            }
-    
-            if(gameBoard.getTurn() >= 5 && gameBoard.winCondition()) //First potential win condition comes at 5th turn
-            {
-                setTimeout(() => 
-                {
-                    announcer.style.display = 'block';
-                    content.getElementsByTagName('h1')[0].innerHTML = `${gameBoard.getPlayer().sign} won!`;
-                    gameBoard.restartGame();
-                    xTurn.style = null;
-                    oTurn.style = null;
-                }, 250);
-            }
-            else if(gameBoard.getTurn() == 10) //Then we have a draw
-            {
-                setTimeout(() => 
-                {
-                    announcer.style.display = 'block';
-                    content.getElementsByTagName('h1')[0].innerHTML = 'Draw!';
-                    gameBoard.restartGame();
-                    xTurn.style = null;
-                    oTurn.style = null;
-                }, 250);
-            } 
-        })
-    }
-}
-
-Game('Player') 
 //Event listeners
 
+board.addEventListener('click', function(e){ 
+
+    // VS PLAYER
+    if(e.target.textContent == '' && game.getGameMode() == 'vsPlayer') //Check if the current square is empty
+    {
+        e.target.textContent = game.getActivePlayer().sign; // Place player's sign on the board
+        game.makeMove(e.target.id, game.getActivePlayer().sign) // Place the sign in the array
+    }
+
+    // VS AI
+    if(e.target.textContent.length == 0 && game.getGameMode() == 'vsAI')
+    {
+        e.target.textContent = playerX.sign; // Place player's sign on the board
+        game.makeMove(e.target.id, 'X')// Place the sign in the array
+        game.aiMakeMove();
+
+    }
+})
+
+// Set gamemode vs Player
 btnPlayer.addEventListener('click', () =>
 {
-    gameBoard.restartGame();
-    xTurn.style = null;
-    oTurn.style = null;
-    Game('Player')
+    game.setGameMode(gameModes.vsPlayer);
+})
+
+// Set gamemode vs AI
+btnAI.addEventListener('click', () =>
+{
+    game.setGameMode(gameModes.vsAI);
 })
 
 btnReplay.addEventListener('click', () => 
